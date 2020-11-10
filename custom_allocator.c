6de47f8dab2b1a540ca40f17d6e96fdc4114f42c
TeuerStein <xenkai123456789@gmail.com>
2020-11-10 21:29:29 +0100

    Added base includes with constants called define. Created a virtual_memory structure. This is a default layout

--- custom_allocator.c
+++ custom_allocator.c
@@ -1,4 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <assert.h>
+
+#define u8 uint8_t
+#define u16 uint16_t
+#define STACK_SIZE 32
+#define HEAP_SIZE STACK_SIZE * 4
+#define HEADER 4
+
+typedef struct virtual_memory {
+    u8 stack[STACK_SIZE];
+    char** unmapped;
+    u8 heap[HEAP_SIZE];
+    
+    struct {
+        char** data;
+        char** bss;
+        char* text;
+    }data_t;
+}virtual_memory_t;
+
+void test() {
+    
+}
 
 int main(int argc, char** argv) {
     return 0;
